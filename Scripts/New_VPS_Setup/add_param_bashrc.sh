407db395b108fd2721887bcf52131f65f8de4f1d
ed-red <edgaremcx@outlook.com>
2023-06-01 16:26:06 -0500

    criando arquivo secrets

--- Scripts/New_VPS_Setup/add_param_bashrc.sh
+++ Scripts/New_VPS_Setup/add_param_bashrc.sh
@@ -7,19 +7,15 @@ yellow=`tput setaf 3`
 reset=`tput sgr0`
 
 #---- inicia script
-# Define as variáveis de ambiente para as APIs das tools
 
-# export SHODAN_API_KEY=xxx
-# export CENSYS_API_ID=xxx
-# export CENSYS_API_SECRET=xxx
-# export FOFA_EMAIL=xxx
-# export FOFA_KEY=xxx
-# export QUAKE_TOKEN=xxx
-# export HUNTER_API_KEY=xxx
-# export ZOOMEYE_API_KEY=xxx
-# export NETLAS_API_KEY=xxx
-# export CRIMINALIP_API_KEY=xxx
-# export PUBLICWWW_API_KEY=xxx
+# Carrega as variáveis do arquivo apis.sh
+source $HOME/Vault_BugBounty/.secrets/apis.sh
 
+# Define a localização do arquivo .bashrc
+bashrc_file="$HOME/.bashrc"
+
+# Adiciona as variáveis ao arquivo .bashrc
 echo "${yellow}[+] Definindo as variáveis com as APIs das tools${reset}"
-echo "export SHODAN_API_KEY=$SHODAN_API_KEY" >> ~/.bashrc
+echo "export SHODAN_API_KEY=$SHODAN_API_KEY" >> "$bashrc_file"
+
+echo "${green}[+] As variáveis foram adicionadas ao arquivo .bashrc com sucesso.${reset}"
