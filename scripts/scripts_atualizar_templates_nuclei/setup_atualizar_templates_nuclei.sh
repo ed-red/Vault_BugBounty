b53e6a531c7a073834424d53e61b65d55319dca3
ed-red <edgaremcx@outlook.com>
2023-06-27 12:57:00 -0300

    script atualizar templates nuclei

---
+++ scripts/scripts_atualizar_templates_nuclei/setup_atualizar_templates_nuclei.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Definindo as cores
+black=`tput setaf 0`
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+blue=`tput setaf 4`
+magenta=`tput setaf 5`
+cyan=`tput setaf 6`
+white=`tput setaf 7`
+reset=`tput sgr0`
+
+# Variaveis
+date="$(date +%d-%m-%Y_%Hh-%Mm-%Ss)"
+
+# Diretório dos scripts
+echo "${yellow}[+] Mudando para o diretório dos scripts...${reset}"
+cd /root/Vault_BugBounty/scripts/scripts_atualizar_templates_nuclei
+
+# Passo 1: Atualizar com o comando newclei
+echo "${yellow}[+] Atualizando com o comando newclei...${reset}"
+newclei -token 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 | anew links.txt | wc -l
+
+# Passo 2: Executar o script Python para puxar os templates
+echo "${yellow}[+] Executando o script Python para puxar os templates...${reset}"
+python3 bot_puxar_templates_nuclei.py
+
+# Passo 3: Ir para o diretório do repositório e dar um pull
+echo "${yellow}[+] Mudando para o diretório do repositório e atualizando...${reset}"
+cd /root/Vault_BugBounty/redmc_custom_templates_nuclei
+pwd
+
+echo "${yellow}[+] git commit -m "Atualização Templates - $date"...${reset}"
+git add .
+git commit -m "Atualização Templates - $date"
+echo "${yellow}[+] git push origin main:main...${reset}"
+git push origin main:main
+
+# Passo 4: Atualizar os templates do nuclei
+echo "${yellow}[+] Atualizando os templates do nuclei...${reset}"
+nuclei -update-templates
